0d0bfa4b4d2ce9747f0df16c562dad5a1e251eb0
dniero95 <darioniero@icloud.com>
2022-09-12 16:05:36 +0200

    add throw method in Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,28 @@
 import React from "react";
 
 class Card extends React.Component {
+    throw = (state) => {
+        const min = 1;
+        const max = 100;
+        const newToHit = state.toHit = min + Math.random() * (max - min);;
+        const newDamage = state.damage = min + Math.random() * (max - min);;
+        this.setState({
+            'toHit': newToHit,
+            'damage': newDamage
+        });
+    }
+
+    state = {
+        'toHit': 0,
+        'damage': 0
+    }
+
     render() {
         return (
             <div className="card">
                 <h5 className="card-header">Nome personaggio</h5>
                 <div className="card-body">
-                    <button className="btn btn-danger">Punch!</button>
+                    <button className="btn btn-danger" onClick={this.throw}>Punch!</button>
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
                         <label class="form-check-label" for="flexCheckDefault">
@@ -14,9 +30,9 @@ class Card extends React.Component {
                         </label>
                     </div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item"></li>
-                        <li class="list-group-item">To hit: </li>
-                        <li class="list-group-item">Damage: </li>
+                        <hr />
+                        <li class="list-group-item">To hit: {this.state.toHit}</li>
+                        <li class="list-group-item">Damage: {this.state.damage}</li>
                     </ul>
                 </div>
 
